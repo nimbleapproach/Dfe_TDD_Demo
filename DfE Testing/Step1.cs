62e87253691485be71d11e2b0951e210d2776111
Yoktech Solutions <steve@yorktechsolutions.co.uk>
2022-02-17 16:23:34 +0000

    more stuff

---
+++ DfE Testing/Step1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using FluentAssertions;
+using Xunit;
+using Xunit.Sdk;
+
+
+namespace DfE_Testing1
+{
+
+   
+    // Some basic requirements
+    // needs a way for customers to report a problem e.g a pothole in the road, flytipping, litter etc
+    // service for recording the report
+
+    // the report needs to go into the councils CRM system
+
+    // need a way to update the customer with a CRM ref number via text / email
+    // and maybe on the outcome of their report (this would be very useful for when a councillor reports a problem as they like to know what's happening
+    //
+
+
+
+    // TDD resources 
+    // Visual Studio Series on Youtube
+    //  https://www.youtube.com/watch?v=HhRvW1b4IwM
+    // C# and other Languages with TDD
+    // https://tdd.tools/
+    // And of course Microsoft docs
+    /// https://docs.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-dotnet-test
+    ///Nice intro to Xunit
+    /// https://auth0.com/blog/xunit-to-test-csharp-code/
+    /// 
+
+    public class Step1
+    {
+        //xUnit supports two different types of unit test, Fact and Theory
+
+
+        // Fact when we have some criteria that always must be met, regardless of data.
+        [Fact]
+        public void Create_A_Pothole_Report_Test()
+        {
+
+            throw (new NotImplementedException());
+
+        }
+
+
+
+
+    }
+
+
+    public abstract class 
+
+    public class Pothole
+    {
+        public string Description { get; set; }
+        public string Lat { get; set; }
+        public string Longitude { get; set; } 
+
+    }
+
+    public interface ICRMService
+    {
+        string AddREport
+    }
+}
---
+++ DfE Testing/Step3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DfE_Testing3
+{
+
+    
+    // Some basic requirements
+    // needs a way for customers to report a problem e.g a pothole in the road, flytipping, litter etc
+    // service for recording the report
+
+    // the report needs to go into the councils CRM system
+
+    // need a way to update the customer with a CRM ref number via text / email
+    // and maybe on the outcome of their report (this would be very useful for when a councillor reports a problem as they like to know what's happening
+    //
+
+
+
+    // TDD resources 
+    // Visual Studio Series on Youtube
+    //  https://www.youtube.com/watch?v=HhRvW1b4IwM
+    // C# and other Languages with TDD
+    // https://tdd.tools/
+    // And of course Microsoft docs
+    /// https://docs.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-dotnet-test
+    ///Nice intro to Xunit
+    /// https://auth0.com/blog/xunit-to-test-csharp-code/
+    /// 
+    public class Step3
+    {
+    }
+}
